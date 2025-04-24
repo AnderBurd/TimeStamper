b5ffeecb784146426ef8b6e9a06c0b7d3325a592
AnderBurd <[AndersonLee0248@gmail.com]>
2025-04-24 16:29:32 -0700

    Set up uploading video, works using POSTMAN

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
-@app.route("/")
+
 @app.route("/process-video", methods=["POST"])
+def process_video():
+    video_file = request.files['video']
 
-video_file = request.files['video']
-upload_path = os.path.join("storedVids", video_file.filename)
-video_file.save(upload_path)
+    upload_path = os.path.join("storedVids", video_file.filename)
+    video_file.save(upload_path)
 
+    timestamps = run_detection(upload_path)
 
-def hello_world():
-    return "<p>Hello world</p>"
+    return jsonify({"timestamps": timestamps})
+
+def run_detection(path):
+    #Test values
+    return ["00:12", "01:03"]  
+
